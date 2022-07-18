de1b5ca893bfcec17c01781a761c014030bedcc6
Rany Alo <rbge82@gmail.com>
2022-07-18 15:35:59 +0200

    modify the page utiles.php

--- libraries/utiles.php
+++ libraries/utiles.php
@@ -29,31 +29,4 @@
           return $errorMsg;
         }
 
-      function verficationp($gps_x, $gps_y, $frequence_sub, $longeur, $largeur,$extension1, $extensions, $size1, $maxSize, $error1,$tmpName1, $name1){
-
-          if(empty($gps_x)){
-          $errorMsg[]="Veuillez saisir le gps_x";	//check mot du passe textbox n'est pas vide
-         }
-          else if(empty($gps_y)){
-           $errorMsg[]="Veuillez saisir le gps_y";	//check nom textbox n'est pas vide 
-          } 
-          else if(empty($frequence_sub)){
-           $errorMsg[]="Veuillez saisir le frequence_sub";	//check email textbox n'est pas vide
-          }
-          else if(empty($longeur)){
-           $errorMsg[]="Veuillez saisir le longeur";	//check  email a un format valide
-          }
-          else if(empty($largeur)){
-           $errorMsg[]="Veuillez saisir le largeur";	//check mot du passe textbox n'est pas vide
-          }
-          elseif (empty($tmpName1) and empty($name1)) {
-            return;
-          }
-          elseif (!in_array($extension1, $extensions) or $size1 > $maxSize or $error1 !== 0) { // check photo
-            $errorMsg[] = "Votre fichier n'est pas une image ou il est plus que 2MO";
-          }
-            return $errorMsg;
-          }
-  
-
 ?>
